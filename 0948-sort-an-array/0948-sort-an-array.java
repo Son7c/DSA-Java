5789f0702b5ab3807f3ea1b4d601bd5f6c55d013
Son7c <souvikmajee039@gmail.com>
2025-08-06 12:08:56 +0530

    Time: 1495 ms (6.76%), Space: 55.9 MB (60.36%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void mergeSort(int[] nums,int beg,int end){
+        int mid;
+        if(beg<end){
+            mid=(beg+end)/2;
+            mergeSort(nums,beg,mid);
+            mergeSort(nums,mid+1,end);
+            merge(nums,beg,mid,end);
+        }
+    }
+    public void merge(int[] nums,int beg,int mid,int end){
+        int i=beg,j=mid+1,index=beg;
+        int[] temp=new int[nums.length];
+        while(i<=mid&&j<=end){
+            if(nums[i]<nums[j]){
+                temp[index]=nums[i];
+                i++;
+            }else{
+                temp[index]=nums[j];
+                j++;
+            }
+            index++;
+        }
+        while(i<=mid){
+            temp[index]=nums[i];
+            i++;
+            index++;
+        }
+        while(j<=end){
+            temp[index]=nums[j];
+            j++;
+            index++;
+        }
+        for(int k=beg;k<index;k++){
+            nums[k]=temp[k];
+        }
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        return nums;
+    }
+}
