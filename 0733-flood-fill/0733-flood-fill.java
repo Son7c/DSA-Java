8fcb9a26df249661cf65a1cb0c163ac670023f0d
Son7c <souvikmajee039@gmail.com>
2025-08-10 20:39:07 +0530

    Time: 2 ms (9.18%), Space: 45.3 MB (19.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -7,24 +7,26 @@ class Solution {
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int origin=image[sr][sc];
-        int[] dr={0,0,1,-1};
-        int dc[]={1,-1,0,0};
+        Queue<Pair> q=new LinkedList<>();
+        int originalCol=image[sr][sc];
+        int[]dr={0,0,1,-1};
+        int[]dc={1,-1,0,0};
         int m=image.length;
         int n=image[0].length;
-        Queue<Pair> q=new LinkedList<>();
-        boolean[][]vis=new boolean[m][n];
         q.add(new Pair(sr,sc));
+        boolean[][]vis=new boolean[m][n];
         int newRow,newCol;
         while(!q.isEmpty()){
             Pair curr=q.remove();
             if(!vis[curr.row][curr.col]){
-                vis[curr.row][curr.col]=true;
                 image[curr.row][curr.col]=color;
+                vis[curr.row][curr.col]=true;
                 for(int d=0;d<4;d++){
                     newRow=curr.row+dr[d];
                     newCol=curr.col+dc[d];
-                    if(newRow>-1 && newRow<m &&newCol>-1&&newCol<n &&!vis[newRow][newCol]&&image[newRow][newCol]==origin) q.add(new Pair(newRow,newCol));
+                    if(newRow>-1 &&newRow<m &&newCol>-1&&newCol<n &&!vis[newRow][newCol]&&image[newRow][newCol]==originalCol){
+                        q.add(new Pair(newRow,newCol));
+                    }
                 }
             }
         }
