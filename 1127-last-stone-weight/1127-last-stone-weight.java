0ac9a5055c48169b46f5ef95bd9df39530d7a217
Son7c <souvikmajee039@gmail.com>
2025-08-18 15:44:18 +0530

    Time: 1 ms (98.36%), Space: 41.1 MB (71.76%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        if(stones.length==1) return stones[0];
+        PriorityQueue <Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<stones.length;i++){
+            pq.add(stones[i]);
+        }
+        while(pq.size()!=1&&!pq.isEmpty()){
+            if(pq.size()==1) return pq.peek();
+            int val=pq.remove()-pq.remove();
+            if(val==0) pq.add(0);
+            else{
+                pq.add(Math.abs(val));
+            }
+        }
+        return pq.peek();
+    }
+}
