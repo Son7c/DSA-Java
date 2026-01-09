d77e2dfaa66d2fdc779445aabf9e1de192ba3a54
Souvik Majee <souvikmajee039@gmail.com>
2026-01-09 21:29:50 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int func(int n,int[] dp){
+        if(n==0||n==1) return 1;
+        if(dp[n]!=0) return dp[n];
+        return dp[n]=func(n-1,dp)+func(n-2,dp);
+    }
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        return func(n,dp);
+    }
+}
