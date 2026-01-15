a5ed348c19787eb7470533743668559964797e57
Souvik Majee <souvikmajee039@gmail.com>
2026-01-15 23:11:27 +0530

    Time: 0 ms (100%), Space: 46.9 MB (98.78%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long m=n;
+        m=Math.abs(m);
+        double ans=1.0;
+        while(m>0){
+            if(m%2!=0){
+                ans=ans*x;
+                m=m-1;
+            }else{
+                m=m/2;
+                x=x*x;
+            }
+        }
+        if(n<0) return 1.0/ans;
+        return ans;
+    }
+}
