b4024956a86f245f6155ebdd5738d0c76c6af256
Souvik Majee <souvikmajee039@gmail.com>
2026-01-30 21:01:21 +0530

    Time: 143 ms (8.68%), Space: 58.3 MB (32.08%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,29 +1,25 @@
 class Solution {
-    public boolean check(int[] stones,int n,int end,HashSet <Integer> hs,int jump,HashMap <String,Boolean> memo){
-        String key=n + "," + jump;
-        if(memo.containsKey(key)) return memo.get(key);
-        if(n==end) return true;
-        if(n>end||n<0||jump<=0) return false;
-        if(!hs.contains(n)){
-            return false;
-        }
-        boolean a=check(stones,n+jump-1,end,hs,jump-1,memo);
-        boolean b=check(stones,n+jump,end,hs,jump,memo);
-        boolean c=check(stones,n+jump+1,end,hs,jump+1,memo);
-        boolean res=a||b||c;
-        memo.put(key,res);
+    public boolean f(int n,int k,int stones[],HashSet<Integer> hs,HashMap<String,Boolean> hm){
+        String key=n + "," + k;
+        if(n==stones[stones.length-1]) return true;
+        if(!hs.contains(n)) return false;
+        if(n>stones[stones.length-1]||k<=0||n<0) return false;
+        if(hm.containsKey(key)) return hm.get(key);
+
+        boolean one=f(n+k-1,k-1,stones,hs,hm);
+        boolean two=f(n+k,k,stones,hs,hm);
+        boolean three=f(n+k+1,k+1,stones,hs,hm);
+        boolean res=one||two||three;
+
+        hm.put(key,res);
         return res;
     }
     public boolean canCross(int[] stones) {
-        if(stones.length > 1 && stones[1] != 1) return false;
-
-        int end=stones[stones.length-1];
-        
-        HashSet <Integer> hs=new HashSet<>();
-        HashMap <String,Boolean> memo=new HashMap<>();
-        for(int i=0;i<stones.length;i++){
-            hs.add(stones[i]);
+        HashSet<Integer> hs=new HashSet<>();
+        HashMap<String,Boolean> hm=new HashMap<>();
+        for(int i:stones){
+            hs.add(i);
         }
-        return check(stones,1,end,hs,1,memo);
+        return f(1,1,stones,hs,hm);
     }
 }
