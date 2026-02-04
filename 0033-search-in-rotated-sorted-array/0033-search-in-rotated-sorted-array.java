0f96282f6fdef8989e9d82c4932fd6c4b483b316
Souvik Majee <souvikmajee039@gmail.com>
2026-02-04 15:20:26 +0530

    Time: 0 ms (100%), Space: 43.7 MB (89.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,24 @@
 class Solution {
-    public int binary(int[] nums,int target,int left, int right){
-        if(left>right){
-            return -1;
-        }
-        int mid=left+((right-left)/2);
-        if(nums[mid]==target){
-            return mid;
-        }
-        if(nums[0]<=nums[mid]){
-            if(target>=nums[left]&& target<=nums[mid]){
-                return binary(nums,target,left,mid-1);
-            }else{
-                return binary(nums,target,mid+1,right);
-            }
-        }else{
-            if(target>=nums[mid]&&target<=nums[right]){
-                return binary(nums,target,mid+1,right);
-            }else{
-                return binary(nums,target,left,mid-1);
-            }
-        }
-    }
     public int search(int[] nums, int target) {
-        return binary(nums,target,0,nums.length-1);
+        int lp=0,rp=nums.length-1;
+        while(lp<=rp){
+            int mid=lp+((rp-lp)/2);
+            if(nums[mid]==target) return mid;
+            //left half is sorted
+            if(nums[lp]<=nums[mid]){
+                if(target>=nums[lp]&&target<=nums[mid]){
+                    rp=mid-1;
+                }else{
+                    lp=mid+1;
+                }
+            }else{
+                if(target>=nums[mid]&&target<=nums[rp]){
+                    lp=mid+1;
+                }else{
+                    rp=mid-1;
+                }
+            }
+        }
+        return -1;
     }
 }
