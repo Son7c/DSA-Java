a2c662ce5f15ccd185c13570752b795fbc9cd0e1
Souvik Majee <souvikmajee039@gmail.com>
2026-02-07 13:05:35 +0530

    Time: 5 ms (38.91%), Space: 43.3 MB (95.67%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count=0;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                count++;
+            }else if(ch==')'){
+                count--;
+            }
+            if(count==1&&ch==')'){
+                sb.append(ch);
+            }else if((count==1&&ch=='(')||count==0){
+                continue;
+            }else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
