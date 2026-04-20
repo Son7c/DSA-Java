ced48fd5fd722cc1f66044c6aa353e406f55fa81
Souvik Majee <souvikmajee039@gmail.com>
2026-04-20 21:05:50 +0530

    Time: 163 ms (5%), Space: 46.8 MB (67.53%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,41 +9,44 @@
  * }
  */
 class Solution {
-    public static ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
-
-        ListNode merged = lists[0];
-        for (int i = 1; i < lists.length; i++) {
-            merged = merge(merged, lists[i]);
-        }
-        return merged;
-    }
-    public static ListNode merge(ListNode head1, ListNode head2) {
-        ListNode mergedLL = new ListNode(-1);
-        ListNode temp = mergedLL;
-        while (head1 != null && head2 != null) {
-            if (head1.val <= head2.val) {
-                temp.next = head1;
-                head1 = head1.next;
-                temp = temp.next;
-            } else {
-                temp.next = head2;
-                head2 = head2.next;
-                temp = temp.next;
+    public ListNode merge(ListNode tempA,ListNode tempB){
+        ListNode res=new ListNode(-1);
+        ListNode temp=res;
+        while(tempA!=null&&tempB!=null){
+            if(tempA.val<=tempB.val){
+                temp.next=tempA;
+                tempA=tempA.next;
+                temp=temp.next;
+            }else{
+                temp.next=tempB;
+                tempB=tempB.next;
+                temp=temp.next;
             }
         }
-        while (head1 != null) {
-            temp.next = head1;
-            head1 = head1.next;
-            temp = temp.next;
+        while(tempA!=null){
+            temp.next=tempA;
+            tempA=tempA.next;
+            temp=temp.next;
         }
-        while (head2 != null) {
-            temp.next = head2;
-            head2 = head2.next;
-            temp = temp.next;
+        while(tempB!=null){
+            temp.next=tempB;
+            tempB=tempB.next;
+            temp=temp.next;
         }
-        return mergedLL.next;
+        return res.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0||(lists.length==1&&lists[0]==null)) return null;
+        if(lists.length==1) return lists[0];
+        ListNode sorted=null;
+        for(int i=0;i<lists.length;i++){
+            if(i==0){
+                sorted=merge(lists[i],lists[i+1]);
+                i=1;
+            }else{
+                sorted=merge(sorted,lists[i]);
+            }
+        }
+        return sorted;
     }
 }
