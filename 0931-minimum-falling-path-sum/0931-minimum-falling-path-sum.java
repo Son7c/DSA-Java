ea176ecdb097cabe216f0f860cd40c298c54569c
Souvik Majee <souvikmajee039@gmail.com>
2026-04-20 20:56:13 +0530

    Time: 11 ms (5.03%), Space: 47.2 MB (50.34%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,31 +1,37 @@
 class Solution {
+    public int f(int i, int j, int[][] matrix, int m, int n, int[][] dp) {
+        if (i >= m || j < 0 || j >= n)
+            return (int) 1e9;
+        if (i == m - 1)
+            return matrix[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int a = matrix[i][j] + f(i + 1, j - 1, matrix, m, n, dp);
+        int b = matrix[i][j] + f(i + 1, j, matrix, m, n, dp);
+        int c = matrix[i][j] + f(i + 1, j + 1, matrix, m, n, dp);
+        return dp[i][j] = Math.min(a, Math.min(b, c));
+    }
+
     public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int[][] dp = new int[n][n];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int dp[][] = new int[m][n];
         for (int i = 0; i < n; i++) {
-            dp[n - 1][i] = matrix[n - 1][i];
+            dp[m - 1][i] = matrix[m - 1][i];
         }
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = m - 2; i >= 0; i--) {
             for (int j = 0; j < n; j++) {
-                int a = Integer.MAX_VALUE;
-                int b = Integer.MAX_VALUE;
-                int c = Integer.MAX_VALUE;
-                if (i < n - 1 && j < n - 1) {
-                    a = matrix[i][j] + dp[i + 1][j + 1];
-                }
-                if (i < n - 1 && j < n) {
-                    b = matrix[i][j] + dp[i + 1][j];
-                }
-                if (i < n - 1 && j > 0) {
-                    c = matrix[i][j] + dp[i + 1][j - 1];
-                }
+                int a=(int)1e9,b=(int)1e9,c=(int)1e9;
+                if(j>0&&i<m-1) a = matrix[i][j] + dp[i + 1][j - 1];
+                if(i<m-1&&j<n)b = matrix[i][j] + dp[i + 1][j];
+                if(j<n-1&&i<m-1)c = matrix[i][j] + dp[i + 1][j + 1];
                 dp[i][j] = Math.min(a, Math.min(b, c));
             }
         }
-        int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            ans = Math.min(ans, dp[0][i]);
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<m;i++){
+            min=Math.min(dp[0][i],min);
         }
-        return ans;
+        return min;
     }
 }
