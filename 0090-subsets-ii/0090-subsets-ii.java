a7fc727f69d81205dd97a485f4df396d3300d18a
Souvik Majee <souvikmajee039@gmail.com>
2026-05-19 21:57:15 +0530

    Time: 3 ms (55.91%), Space: 45.4 MB (10.11%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public void h(int idx, int[] nums, HashSet<List<Integer>> set, List<List<Integer>> res, List<Integer> path) {
-        if (!set.contains(path)) {
-            set.add(new ArrayList<>(path));
-            res.add(new ArrayList<>(path));
-        }
         for (int i = idx; i < nums.length; i++) {
             path.add(nums[i]);
+            if (!set.contains(path)) {
+                set.add(new ArrayList<>(path));
+                res.add(new ArrayList<>(path));
+            }
             h(i + 1, nums, set, res, path);
             path.remove(path.size() - 1);
         }
@@ -17,6 +17,7 @@ class Solution {
         HashSet<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
         h(0, nums, set, res, path);
+        res.add(new ArrayList<>());
         return res;
     }
 }
