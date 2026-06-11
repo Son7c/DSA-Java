4b8c30ac21662792947dfbb228212ea79de24378
Souvik Majee <souvikmajee039@gmail.com>
2026-06-11 21:33:51 +0530

    Time: 1 ms (30.15%), Space: 46.7 MB (15.96%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -8,53 +8,54 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
-    public ListNode revLL(ListNode head){
-        if(head==null||head.next==null) return head;
-        ListNode newHead=revLL(head.next);
-        ListNode next=head.next;
-        next.next=head;
-        head.next=null;
+    public ListNode revLL(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode newHead = revLL(head.next);
+        ListNode next = head.next;
+        next.next = head;
+        head.next = null;
         return newHead;
     }
+
     public ListNode reverseKGroup(ListNode head, int k) {
-        if(head.next==null) return head;
-        ListNode temp=head;
-        int length=0;
-        while(temp!=null){
-            length++;
-            temp=temp.next;
-        }
-        if(length==k){
-            return revLL(head);
-        }
-        ArrayList<ListNode> map=new ArrayList<>();
+        if(head==null||head.next==null) return head;
         int count=1;
-        temp=head;
-        ListNode start=head;
-        ListNode next=null;
-        while(temp!=null){
+        ListNode temp=head,start=head,next=null;
+        List<ListNode> list=new ArrayList<>();
+        List<ListNode> revL=new ArrayList<>();
+        while(temp!=null&&temp.next!=null){
             if(count%k==0){
-                System.out.println(start.val);
-                map.add(start);
+                list.add(start);
                 next=temp.next;
                 temp.next=null;
+                start=next;
                 count++;
                 temp=next;
-                start=temp;
             }else{
                 count++;
                 temp=temp.next;
             }
         }
-        ArrayList <ListNode> rev=new ArrayList<>();
-        for(int i=0;i<map.size();i++){
-            rev.add(revLL(map.get(i)));
+        if(count%k==0) list.add(start);
+        for(int i=0;i<list.size();i++){
+            ListNode rev=revLL(list.get(i));
+            revL.add(rev);
         }
-        for(int i=0;i<map.size()-1;i++){
-            map.get(i).next=rev.get(i+1);
+        boolean flag=false;
+        for(int i=0;i<list.size()-1;i++){
+            if(i==0){
+                head=revL.get(i);
+                flag=true;
+            }
+            list.get(i).next=revL.get(i+1);
         }
-        map.get(map.size()-1).next=next;
-        return rev.get(0);
+        if(count%k!=0) list.get(list.size()-1).next=start;
+        if(!flag){
+            head=revL.get(0);
+        }
+        return head;
     }
 }
