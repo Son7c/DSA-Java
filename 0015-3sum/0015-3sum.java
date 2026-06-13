aa7bc032a004b55fc1ba0bad9c38d6efbdbd3733
Souvik Majee <souvikmajee039@gmail.com>
2026-06-13 21:50:29 +0530

    Time: 37 ms (24.1%), Space: 59.4 MB (24.68%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            int l = i + 1, r = nums.length - 1;
-            while (l < r) {
-                if (nums[l] + nums[r] == -nums[i]) {
-                    List<Integer> temp = new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r]));
-                    set.add(temp);
+        for(int i=0;i<nums.length;i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int l=i+1,r=nums.length-1;
+            while(l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    List<Integer> temp=new ArrayList<>(Arrays.asList(nums[i],nums[l],nums[r]));
+                    ans.add(temp);
                     l++;
                     r--;
-                } else if (nums[l] + nums[r] < -nums[i])
-                    l++;
-                else
-                    r--;
-            }
+                    while(l<r&&nums[l-1]==nums[l]) l++;
+                    while(l<r&&nums[r+1]==nums[r]) r--;
+                }
+                else if(sum<0) l++;
+                else r--;
+            }   
         }
-        return new ArrayList<>(set);
+        return ans;
     }
 }
