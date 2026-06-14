11e73509cc30c607779f1a46d2071ffb398e9a6e
Souvik Majee <souvikmajee039@gmail.com>
2026-06-14 12:46:38 +0530

    Time: 1 ms (59.24%), Space: 49 MB (9.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,45 +1,30 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-        if(n<=2) return 0;
-        Stack<Integer> st=new Stack<>();
         int[] nge=new int[n];
         int[] pge=new int[n];
+        int max=height[n-1];
         nge[n-1]=-1;
-        st.push(n-1);
         for(int i=n-2;i>=0;i--){
-            while(!st.isEmpty()&&height[st.peek()]<=height[i]) st.pop();
-            if(st.isEmpty()){
+            if(height[i]>=max){
                 nge[i]=-1;
-                st.push(i);
-                continue;
+                max=height[i];
             }
-            if(height[st.peek()]>height[i]&&nge[st.peek()]!=-1) nge[i]=nge[st.peek()];
-            else nge[i]=st.peek();
-            st.push(i);
+            else nge[i]=max;
         }
-        st.clear();
         pge[0]=-1;
-        st.push(0);
-        for(int i=1;i<n;i++){
-            while(!st.isEmpty()&&height[st.peek()]<=height[i]) st.pop();
-            if(st.isEmpty()){
+        max=height[0];
+        for(int i=0;i<n;i++){
+            if(height[i]>=max){
                 pge[i]=-1;
-                st.push(i);
-                continue;
+                max=height[i];
             }
-            if(height[st.peek()]>height[i]&&pge[st.peek()]!=-1) pge[i]=pge[st.peek()];
-            else pge[i]=st.peek();
-            st.push(i);
+            else pge[i]=max;
         }
         int ans=0;
         for(int i=0;i<n;i++){
-            if(nge[i]==-1||pge[i]==-1){
-                ans+=0;
-                continue;
-            }
-            int waterLevel=Math.min(height[nge[i]],height[pge[i]]);
-            ans+=waterLevel-height[i];
+            if(nge[i]==-1||pge[i]==-1) continue;
+            ans+=Math.min(nge[i],pge[i])-height[i];
         }
         return ans;
     }
