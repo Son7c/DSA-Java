0c488cef9c0dd28bbafab433a3f911d4b3cd7093
Souvik Majee <souvikmajee039@gmail.com>
2026-06-16 22:09:12 +0530

    Time: 29 ms (20.91%), Space: 47.1 MB (23.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,24 @@
 class Solution {
-    int f(int i,int amt,int[]coins){
-        if(i==0){
-            if(amt%coins[i]==0) return amt/coins[0];
+    int f(int idx,int[] coins,int amt,int[][]dp){
+        if(idx==0){
+            if(amt%coins[idx]==0) return amt/coins[0];
             else return (int)1e9;
         }
-        int notTake=f(i-1,amt,coins);
+        if (amt == 0) return 0;
+        if(dp[idx][amt]!=-1) return dp[idx][amt];
+        int notTake=f(idx-1,coins,amt,dp);
         int take=(int)1e9;
-        if(coins[i]<=amt){
-            take=1+f(i,amt-coins[i],coins);
+        if(coins[idx]<=amt){
+            take=1+f(idx,coins,amt-coins[idx],dp);
         }
-        return Math.min(take,notTake);
+        return dp[idx][amt]=Math.min(take,notTake);
     }
     public int coinChange(int[] coins, int amount) {
-        int n=coins.length,m=amount+1;
-        int[][]dp=new int[n][m];
-        for(int i=0;i<m;i++){
-            if(i%coins[0]==0) dp[0][i]=i/coins[0];
-            else dp[0][i]=(int)1e9;
+        int[][]dp=new int[coins.length][amount+1];
+        for(int i=0;i<coins.length;i++){
+            Arrays.fill(dp[i],-1);
         }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-                int notTake=dp[i-1][j];
-                int take=(int)1e9;
-                if(coins[i]<=j){
-                    take=1+dp[i][j-coins[i]];
-                }
-                dp[i][j]=Math.min(take,notTake);
-            }
-        }
-        return dp[n-1][amount]>=(int)1e9?-1:dp[n-1][amount];
+        int val=f(coins.length-1,coins,amount,dp);
+        return val>=(int)1e9?-1:val;
     }
 }
