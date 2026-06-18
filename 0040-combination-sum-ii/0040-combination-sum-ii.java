e645b483a43925e9b265651c55636faf56e1e858
Souvik Majee <souvikmajee039@gmail.com>
2026-06-18 21:53:00 +0530

    Time: 6 ms (72.88%), Space: 45 MB (86.7%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,26 +1,23 @@
 class Solution {
-    public void h(int idx, List<List<Integer>> res, List<Integer> path, int[] nums, int target) {
-        if (target == 0) {
-            res.add(new ArrayList<>(path));
+    void f(int i, int target, int[] nums, List<Integer> path, List<List<Integer>> list) {
+        if(target==0){
+            list.add(new ArrayList<>(path));
             return;
         }
-        for (int i = idx; i < nums.length; i++) {
-            if(i>idx&&nums[i]==nums[i-1]){
-                continue;
-            }
-            if (nums[i] > target)
-                break;
-            path.add(nums[i]);
-            h(i + 1, res, path, nums, target - nums[i]);
-            path.remove(path.size() - 1);
+        for(int j=i;j<nums.length;j++){
+            if(j>i&&nums[j]==nums[j-1]) continue;
+            if(nums[j]>target) break;
+            path.add(nums[j]);
+            f(j+1,target-nums[j],nums,path,list);
+            path.remove(path.size()-1);
         }
     }
 
-    public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<Integer> path = new ArrayList<>();
-        h(0, res, path, nums, target);
-        return res;
+        List<List<Integer>> list = new ArrayList<>();
+        f(0, target, candidates, path, list);
+        return list;
     }
 }
