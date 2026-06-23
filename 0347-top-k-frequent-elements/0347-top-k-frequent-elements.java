904c8f353bfd89a589d565ddc9ea3c5ce1a798ac
Souvik Majee <souvikmajee039@gmail.com>
2026-06-23 22:08:38 +0530

    Time: 15 ms (55.44%), Space: 47.5 MB (65.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,30 @@
- class Solution {
-    class Pair implements Comparable<Pair> {
-        int idx, count;
-        public Pair(int i, int c) {
-            this.idx = i;
-            this.count = c;
-        }
-        @Override
-        public int compareTo(Pair p2) {
-            return p2.count - this.count;
-        }
+class Pair implements Comparable<Pair>{
+    int num,count;
+    public Pair(int n,int c){
+        this.num=n;
+        this.count=c;
     }
-
+    @Override
+    public int compareTo(Pair p2){
+        return p2.count-this.count;
+    }
+}
+class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        PriorityQueue<Pair> pq=new PriorityQueue<>();
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        Set<Integer> keys = map.keySet();
-        for (Integer i : keys) {
-            pq.add(new Pair(i, map.get(i)));
+        for(int key:hm.keySet()){
+            pq.add(new Pair(key,hm.get(key)));
         }
-
-        int[] ans = new int[k];
-        int count = 0;
-        while (!pq.isEmpty()) {
-            if (count == k) break;
-            ans[count++] = pq.remove().idx;
+        int arr[]=new int[k];
+        int i=0;
+        while(k>0){
+            arr[i++]=pq.poll().num;
+            k--;
         }
-        return ans;
+        return arr;
     }
- }
+}
