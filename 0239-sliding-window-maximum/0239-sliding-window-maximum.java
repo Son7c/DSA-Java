9532d9b0cf0f9318cd858adb4c635c6620e95345
Souvik Majee <souvikmajee039@gmail.com>
2026-06-27 22:04:56 +0530

    Time: 27 ms (94.27%), Space: 146.5 MB (68.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,21 +1,23 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> dq=new ArrayDeque<>();
-        int ans[]=new int[nums.length-k+1];
-        int idx=0;
-
-        for(int i=0;i<nums.length;i++){
-            if(!dq.isEmpty()&& dq.peekFirst()==i-k){
-                dq.pollFirst();
-            }
-            while(!dq.isEmpty()&& nums[dq.peekLast()]<nums[i]){
-                dq.pollLast();
-            }
+        int n=nums.length;
+        int[]res=new int[n-k+1];
+        
+        for(int i=0;i<k;i++){
+            while(!dq.isEmpty()&&nums[i]>nums[dq.peekLast()]) dq.pollLast();
             dq.addLast(i);
-            if(i>=k-1){
-                ans[idx++]=nums[dq.peekFirst()];
-            }
         }
-        return ans;
+        res[0]=nums[dq.peekFirst()];
+        for(int i=k;i<n;i++){
+            if(dq.peekFirst()<i-k+1) dq.pollFirst();
+            while(!dq.isEmpty()&&nums[i]>nums[dq.peekLast()])
+            dq.pollLast();
+            
+            dq.addLast(i);
+            
+            res[i-k+1]=nums[dq.peekFirst()];
+        }
+        return res;
     }
 }
