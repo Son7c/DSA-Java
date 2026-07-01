10991c4fbd48f8fad256ae01753511f55074f921
Souvik Majee <souvikmajee039@gmail.com>
2026-07-01 21:46:14 +0530

    Time: 3 ms (9.43%), Space: 43.2 MB (33.82%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,36 +1,39 @@
 class Solution {
-    public int strStr(String text, String pat) {
-        int[]lps=lps(pat);
-        int i=0,j=0,n=text.length(),m=pat.length();
+    private int[] lps(String needle){
+        int m=needle.length();
+        int[]lps=new int[m];
+        lps[0]=0;
+        int i=1,length=0;
+        while(i<m){
+            if(needle.charAt(length)==needle.charAt(i)){
+                length++;
+                lps[i]=length;
+                i++;
+            }
+            else if(length!=0){
+                length=lps[length-1];
+            }else{
+                lps[i]=0;
+                i++;
+            }
+        }
+        return lps;
+    }
+    public int strStr(String haystack, String needle) {
+        int[]lps=lps(needle);
+        int i=0,j=0;
+        int n=haystack.length(),m=needle.length();
         while(i<n){
-            if(text.charAt(i)==pat.charAt(j)){
+            if(haystack.charAt(i)==needle.charAt(j)){
                 i++;
                 j++;
             }
             if(j==m) return i-m;
-            if(i<n&&text.charAt(i)!=pat.charAt(j)){
+            if(i<n&&haystack.charAt(i)!=needle.charAt(j)){
                 if(j!=0) j=lps[j-1];
                 else i++;
             }
         }
         return -1;
-    }
-    public int[] lps(String pat){
-        int m=pat.length();
-        int[] lps=new int[m];
-        int prev=0,i=1;
-        lps[0]=0;
-        while(i<m){
-            if(pat.charAt(i)==pat.charAt(prev)){
-                prev++;
-                lps[i]=prev;
-                i++;
-            }else if(prev!=0){
-                prev=lps[prev-1];
-            }else{
-                i++;
-            }
-        }
-        return lps;
     }
 }
