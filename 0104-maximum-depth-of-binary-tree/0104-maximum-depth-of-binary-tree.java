3c1bb163eae0247511debec3c033d240bdc7316f
Souvik Majee <souvikmajee039@gmail.com>
2026-07-06 15:20:52 +0530

    Time: 0 ms (100%), Space: 46.9 MB (5.05%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,12 +14,14 @@
  * }
  */
 class Solution {
+    public int inorder(TreeNode root){
+        if(root==null) return 0;
+        int left=1+inorder(root.left);
+        int right=1+inorder(root.right);
+        return Math.max(left,right);
+    }
     public int maxDepth(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        int left=maxDepth(root.left);
-        int right=maxDepth(root.right);
-        return Math.max(left,right)+1;
+        if(root==null) return 0;
+        return inorder(root);
     }
 }
