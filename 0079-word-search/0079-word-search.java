7b05c0e786dadf61d6dd64864885ad42e35239d7
Souvik Majee <souvikmajee039@gmail.com>
2026-07-30 23:00:46 +0530

    Time: 140 ms (47.94%), Space: 42.9 MB (60.59%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,34 +1,26 @@
 class Solution {
-    public boolean dfs(char[][] board, String word, int r, int c, int i, int[] dr, int[] dc, boolean[][] vis) {
-        if (board[r][c] == word.charAt(i)) {
-            vis[r][c] = true;
-        } else
-            return false;
-        boolean ans = false;
-        if (i == word.length()-1)
+    public boolean dfs(char[][] board, String word, int r, int c, int i, boolean[][] vis) {
+        if (i == word.length())
             return true;
-        for (int j = 0; j < 4; j++) {
-            int newRow = r + dr[j], newCol = dc[j] + c;
-            if (newRow < board.length && newRow >= 0 && newCol < board[0].length && newCol >= 0
-                    && !vis[newRow][newCol]) {
 
-                ans = dfs(board, word, newRow, newCol, i + 1, dr, dc, vis);
-                if (ans)
-                    break;
-            }
-        }
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || word.charAt(i) != board[r][c] || vis[r][c])
+            return false;
+
+        vis[r][c] = true;
+
+        boolean found = dfs(board, word, r, c - 1, i + 1, vis) || dfs(board, word, r, c + 1, i + 1, vis)
+                || dfs(board, word, r - 1, c, i + 1, vis) || dfs(board, word, r + 1, c, i + 1, vis);
+
         vis[r][c] = false;
-        return ans;
+        return found;
     }
 
     public boolean exist(char[][] board, String word) {
-        int[] dr = { -1, 1, 0, 0 };
-        int[] dc = { 0, 0, 1, -1 };
         int m = board.length, n = board[0].length;
         boolean[][] vis = new boolean[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0, dr, dc, vis))
+                if (board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0, vis))
                     return true;
             }
         }
