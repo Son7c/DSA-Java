91548ed93ed1390074b5d1018fa41d0d7d23f18b
Souvik Majee <souvikmajee039@gmail.com>
2026-08-09 13:49:04 +0530

    Time: 291 ms (48.2%), Space: 98.1 MB (61.71%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,45 +1,43 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
+        int mod=(int) 1e9+7;
         int n=arr.length;
-        int[]nse=new int[n];
-        int[]pse=new int[n];
-        nse[n-1]=n;
-        Stack<Integer> st=new Stack<>();
+        Stack <Integer> st=new Stack<>();
+        int[] nse=new int[n];
+        int[] pse=new int[n];
+
+        //nse
         st.push(n-1);
+        nse[n-1]=n;
         for(int i=n-2;i>=0;i--){
-            if(arr[st.peek()]<arr[i]){
+            while(!st.isEmpty()&&arr[st.peek()]>=arr[i]) st.pop();
+            if(st.isEmpty()){
+                nse[i]=n;
+            }else{
                 nse[i]=st.peek();
-            }else{
-                while(!st.isEmpty()&&arr[st.peek()]>arr[i]){
-                    st.pop();
-                }
-                if(st.isEmpty()) nse[i]=n;
-                else nse[i]=st.peek();
             }
             st.push(i);
         }
-        pse[0]=-1;
         st.clear();
+
+        //pse
         st.push(0);
+        pse[0]=-1;
         for(int i=1;i<n;i++){
-            if(arr[st.peek()]<arr[i]){
-                pse[i]=st.peek();
+            while(!st.isEmpty()&&arr[st.peek()]>arr[i]) st.pop();
+            if(st.isEmpty()){
+                pse[i]=-1;
             }else{
-                while(!st.isEmpty()&&arr[st.peek()]>=arr[i]){
-                    st.pop();
-                }
-                if(st.isEmpty()) pse[i]=-1;
-                else pse[i]=st.peek();
+                pse[i]=st.peek();
             }
             st.push(i);
         }
-        long sum=0;
-        int mod=(int)1e9+7;
+
+        long ans=0l;
         for(int i=0;i<n;i++){
-            int left=i-pse[i];
-            int right=nse[i]-i;
-            sum = (sum + (long) left * right * arr[i]) % mod;
+            int left=i-pse[i],right=nse[i]-i;
+            ans=(ans+((long) left*right*arr[i])%mod)%mod;
         }
-        return (int)sum;
+        return (int) ans;
     }
 }
