84832236e80606076be14d934bfa00c5cf36c5ad
Souvik Majee <souvikmajee039@gmail.com>
2026-08-17 13:14:12 +0530

    Time: 111 ms (13.43%), Space: 77.8 MB (63.91%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,37 +1,28 @@
 class Solution {
-    public int largestRectangleArea(int[] nums) {
-        int n=nums.length;
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
         int[] nse=new int[n];
         int[] pse=new int[n];
+        int min=Integer.MAX_VALUE;
         Stack<Integer> st=new Stack<>();
-        nse[n-1]=n;
-        st.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(!st.isEmpty()&&nums[st.peek()]>=nums[i]) st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&&heights[st.peek()]>=heights[i]) st.pop();
             if(st.isEmpty()){
                 nse[i]=n;
-                st.push(i);
-                continue;
-            }
-            nse[i]=st.peek();
+            }else nse[i]=st.peek();
             st.push(i);
         }
         st.clear();
-        pse[0]=-1;
-        st.push(0);
-        for(int i=1;i<n;i++){
-            while(!st.isEmpty()&&nums[st.peek()]>=nums[i]) st.pop();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&&heights[st.peek()]>=heights[i]) st.pop();
             if(st.isEmpty()){
                 pse[i]=-1;
-                st.push(i);
-                continue;
-            }
-            pse[i]=st.peek();
+            }else pse[i]=st.peek();
             st.push(i);
         }
         int ans=0;
         for(int i=0;i<n;i++){
-            ans=Math.max(ans,(nse[i]-pse[i]-1)*nums[i]);
+            ans=Math.max(ans,heights[i]*(nse[i]-pse[i]-1));
         }
         return ans;
     }
