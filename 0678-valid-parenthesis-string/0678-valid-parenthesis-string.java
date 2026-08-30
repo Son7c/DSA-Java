3cb38211d2cb3559479d8a85434c7527c4cc949b
Souvik Majee <souvikmajee039@gmail.com>
2026-08-30 15:51:58 +0530

    Time: 7 ms (1.9%), Space: 43.3 MB (9.12%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,22 +1,34 @@
 class Solution {
-    public boolean checkValidString(String s) {
-        int min = 0, max = 0;
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                min += 1;
-                max += 1;
-            } else if (ch == ')') {
-                min -= 1;
-                max -= 1;
-            } else {
-                min -= 1;
-                max += 1;
-            }
-            if (min < 0)
-                min = 0;
-            if (max < 0)
-                return false;
+    public boolean f(String s, int idx, int ct) {
+        if (idx == s.length()) {
+            return ct == 0;
         }
-        return min == 0;
+        if (ct < 0)
+            return false;
+        if (s.charAt(idx) == '(')
+            return f(s, idx + 1, ct + 1);
+        else if (s.charAt(idx) == ')')
+            return f(s, idx + 1, ct - 1);
+        return f(s, idx + 1, ct + 1) || f(s, idx + 1, ct - 1) || f(s, idx + 1, ct);
+    }
+
+    public boolean checkValidString(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n + 1][n + 1];
+        dp[n][0] = true;
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int ct = 0; ct <= n; ct++) {
+                if (s.charAt(idx) == '(') {
+                    if (ct + 1 <= n)
+                        dp[idx][ct] = dp[idx + 1][ct + 1];
+                } else if (s.charAt(idx) == ')') {
+                    if (ct - 1 >= 0)
+                        dp[idx][ct] = dp[idx + 1][ct - 1];
+                } else {
+                    dp[idx][ct] = ct+1<=n&& dp[idx + 1][ct + 1] ||ct-1>=0&& dp[idx + 1][ct - 1] || dp[idx + 1][ct];
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
