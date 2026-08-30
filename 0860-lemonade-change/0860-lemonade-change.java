dda84a1ba2b6758cc33e2d5c865a8bcff7ae0f21
Souvik Majee <souvikmajee039@gmail.com>
2026-08-30 12:36:30 +0530

    Time: 17 ms (4.76%), Space: 81.8 MB (5.55%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(5,0);
+        map.put(10,0);
+        map.put(20,0);
+        for(int i:bills){
+            if(i==5) map.put(i,map.get(i)+1);
+            else if(i==10){
+                if(map.get(5)>0){
+                    map.put(5,map.get(5)-1);
+                    map.put(i,map.get(10)+1);
+                }else return false;
+            }else{
+                if(map.get(10)>0&&map.get(5)>0){
+                    map.put(10,map.get(10)-1);
+                    map.put(5,map.get(5)-1);
+                    map.put(i,map.get(20)+1);
+                }else if(map.get(5)>=3){
+                    map.put(5,map.get(5)-3);
+                    map.put(i,map.get(20)+1);
+                }else return false;
+            }
+        }
+        return true;
+    }
+}
