7a8ab9cf75ba0288af6023b23040cf01e48f0c80
Souvik Majee <souvikmajee039@gmail.com>
2026-08-31 21:52:30 +0530

    Time: 2 ms (89.03%), Space: 47.9 MB (62.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -10,19 +10,29 @@ class Solution {
     //     return dp[idx]=ans;
     // }
     public boolean canJump(int[] nums) {
+        //Memoization
         // if(nums.length==1) return true;
         // Boolean[] dp=new Boolean[nums.length];
         // return f(nums,0,dp);
-        int n=nums.length;
-        boolean[] dp=new boolean[n];
-        dp[n-1]=true;
-        for(int i=n-2;i>=0;i--){
-            boolean ans=false;
-            for(int j=1;j<=nums[i];j++){
-                if(i+1<n) ans=ans||dp[i+j];
-            }
-            dp[i]=ans;
+
+
+        //Tabulation
+        // int n=nums.length;
+        // boolean[] dp=new boolean[n];
+        // dp[n-1]=true;
+        // for(int i=n-2;i>=0;i--){
+        //     boolean ans=false;
+        //     for(int j=1;j<=nums[i];j++){
+        //         if(i+1<n) ans=ans||dp[i+j];
+        //     }
+        //     dp[i]=ans;
+        // }
+        // return dp[0];
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max) return false;
+            max=Math.max(max,nums[i]+i);
         }
-        return dp[0];
+        return true;
     }
 }
